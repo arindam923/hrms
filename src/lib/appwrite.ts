c232fdd5af40bb4ca09b45a4dea50af4eb1fdfa8
arindam923 <arindam92@proton.me>
2024-11-15 11:33:53 +0530

    initialize the appwrite correct way

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,4 +1,36 @@
-import { Client } from "appwrite";
+"use server";
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers";
 
-export const client = new Client();
-client.setProject("6736e10300157bf739a9");
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+  const cook = await cookies();
+  const session = cook.get("my-custom-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
